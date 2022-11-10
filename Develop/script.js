f1a4c896fcace76c625f636f896ad6f5a6d14b43
mohamed-aden <aden6864@bears.unco.edu>
2022-11-09 19:41:41 -0700

    updated the dates dynamically

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
+const currentDayEl = $('#currentDay');
+const containerEl = $('.container-lg');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const now = dayjs().format('dddd, MMMM DD');
+
+currentDayEl.text(now);
+
+//Monday, December 13th
+console.log(now);
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
