033497c976b8da496dfcf2e8c917f99da6e95f06
mohamed-aden <aden6864@bears.unco.edu>
2022-11-10 18:08:02 -0700

    Uppdate the README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,17 @@
 const currentDayEl = $('#currentDay');
 const containerEl = $('.container-lg');
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
+//Time var for business hours 9AM - 5PM
 const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-
 const militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-//Update the var to the main container
-// timeRow.text('4PM');
-// hourRow.append(timeRow);
-// hourRow.append(descRow);
-// hourRow.append(saveBtn);
 
 const now = dayjs();
 let scheduledItems = {};
 
+//Display time => Monday, December 13th
 currentDayEl.text(now.format('dddd, MMMM DD'));
 
-//Monday, December 13th
+//Hours 0-24
 const hourNow = now.format('H');
 
 //func that creates new el / conatiner
@@ -60,16 +53,14 @@ function createHourElement() {
   }
 }
 
+//Call the func
 createHourElement();
 
 function loadScheduledData() {
+  //get the data from localStorage & if its empty create new empty obj
   scheduledItems = JSON.parse(localStorage.getItem('scheduledItems')) || {};
 
-  // console.log(scheduledItems);
-
   $.each(scheduledItems, function (time, text) {
-    // console.log(time, text);
-
     const hourBlock = $('.time-block')
       .find('.hour')
       .filter(function () {
@@ -81,6 +72,7 @@ function loadScheduledData() {
   });
 }
 
+//Call the fun
 loadScheduledData();
 
 $(function () {
@@ -94,26 +86,9 @@ $(function () {
     //time as prop and text val
     scheduledItems[time] = text;
 
+    //Store the txt & time in local storage
     localStorage.setItem('scheduledItems', JSON.stringify(scheduledItems));
 
     // console.log(scheduledItems);
   });
-
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work-Day-Scheduler
+
+---
+
+### Create online work schedular for your busy business days.
+
+[Schedule your Workdays now 🟩](https://mo-aden.github.io/Work-Day-Scheduler/Develop/)
+
+### Demo of the web Applications
+
+![App Demo]("./Assets/appDemo.png)
